ad9de0d237f10f0d4729a33034298b4b4c24e7c6
connor <70295743+connorhedgehog@users.noreply.github.com>
2025-07-16 23:23:48 -0500

    Updated ATM

--- prog/ATM.lua
+++ prog/ATM.lua
@@ -1,44 +1,73 @@
 --ATM
-while not disk.isPresent("bottom") do
-    sleep(0)
+print("Please insert a cc-casino branded disk!")
+while not fs.exists("/disk/money.lua") do
+    sleep()
 end
-print("D or W")
-local A = io.read()
+
+print("Would you like to [d]eposit, [w]ithdraw, or check your [b]alance?")
+local answer = io.read()
 local db = fs.open("/disk2/log.log", "a")
 local id = disk.getID("bottom")
-if A == "D" then
-    local I = turtle.getItemDetail()
-    if I.name ~= "minecraft:iron_ingot" or I.name == nil then
-        print("Please insert iron ingot")
+if answer == "d" or answer == "deposit" then
+    local item = turtle.getItemDetail()
+    if item.name ~= "minecraft:iron_ingot" or item.name == nil then
+        print("Please insert iron ingots!")
         sleep(1)
         shell.run("reboot")
     end
-    local dims = turtle.getItemCount()
-    local disk = fs.open("/disk/money.lua", "r")
-    dims = dims
-    money = disk.readAll()
-    disk.close()
-    money = tonumber(money)
-    money = money + dims
+
+    local ironCount = turtle.getItemCount()
+    local card = fs.open("/disk/money.lua", "r")
+    local money = tonumber(card.readAll())
+
+    card.close()
+
+    money = money + ironCount
     turtle.drop()
-    disk = fs.open("/disk/money.lua", "w")
-    disk.write(money)
-    db.write(dims..id.."\n")
-    disk.close()
-    print("you have $", money, "Now!!" )
-    sleep(3)
-elseif A == "W" then
-    print("amount of ingots to withdraw")
-    A = io.read()
-    disk = fs.open("/disk/money.lua", "r")
-    money = disk.readAll()
-    disk.close()
-    money = money - A
-    turtle.suck(A)
-    db.write(A..id.."\n")
-    disk = fs.open("/disk/money.lua", "w")
-    disk.write(money)
-    disk.close()
+
+    card = fs.open("/disk/money.lua", "w")
+    card.write(money)
+    db.write(ironCount..id.."\n")
+    card.close()
+    print("Your balance is now $"..money)
+    
+    sleep(2)
+elseif answer == "w" or answer == "withdraw" then
+    print("How much would you like to withdraw?")
+    answer = tonumber(io.read())
+
+    local card = fs.open("/disk/money.lua", "r")
+    local money = tonumber(card.readAll())
+
+    card.close()
+
+    if answer > money then
+        print("That's more than you have! Try again.")
+
+        sleep(2)
+    else
+        money = money - answer
+
+        print("You now have $"..money)
+
+        turtle.suck(answer)
+        db.write(answer..id.."\n")
+        card = fs.open("/disk/money.lua", "w")
+        card.write(money)
+        card.close()
+
+        sleep(2)
+    end
+elseif answer == "b" or answer == "balance" then
+    local card = fs.open("/disk/money.lua", "r")
+    local money = tonumber(card.readAll())
+
+    print("Your balance is $"..money)
+
+    card.close()
+
+    sleep(2)
 end
+
 db.close()
 shell.run("reboot")
